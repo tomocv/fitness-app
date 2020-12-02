7ef9b81dddb2ee66a40e5176cce2dc178ae7928b
tomocv <tcvenic@gmail.com>
2020-12-02 19:32:06 +0100

    Initial project setup

---
+++ src/test/java/com/tomocv/fitnessapp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomocv.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
